18381ad4d98ceed358f1ebf57c0c210456189cb4
JhonFac <88755343+JhonFac@users.noreply.github.com>
2026-05-03 15:57:56 -0500

    update start container

--- reiniciar_contenedores.sh
+++ reiniciar_contenedores.sh
@@ -2,18 +2,16 @@
 
 echo "[$(date)] - Iniciando proceso de recreación con Docker Compose"
 
-# Ruta al archivo docker-compose.yml (ajústala según sea necesario)
 DOCKER_COMPOSE_FILE="docker-compose.yml"
 
-# Pasos para eliminar y recrear los contenedores
 echo "[$(date)] - Deteniendo y eliminando servicios existentes"
-docker-compose -f "$DOCKER_COMPOSE_FILE" down || {
+docker compose -f "$DOCKER_COMPOSE_FILE" down || {
     echo "[$(date)] - Error al detener servicios"
     exit 1
 }
 
 echo "[$(date)] - Recreando y levantando servicios"
-docker-compose -f "$DOCKER_COMPOSE_FILE" up -d || {
+docker compose -f "$DOCKER_COMPOSE_FILE" up -d --build || {
     echo "[$(date)] - Error al recrear servicios"
     exit 1
 }
